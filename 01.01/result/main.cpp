b334130463dcb6f101e7f5e5a20a50a4f23ddcd5
fink <hspan1234@gmail.com>
2013-05-20 21:17:52 +0800

    更改文件夹名称及更新01.02答案

---
+++ 01.01/result/main.cpp
@@ -0,0 +1,28 @@
+int main()
+{
+    return 0;
+}
+/*********************************************
+g++    -c /home/fink/fink/cpp_primer_exercises/01.01/答案/main.cpp -o /home/fink/fink/cpp_primer_exercises/01.01/答案/main.o
+g++  -o /home/fink/fink/cpp_primer_exercises/01.01/答案/main /home/fink/fink/cpp_primer_exercises/01.01/答案/main.o
+Process terminated with status 0 (0 minutes, 0 seconds)
+0 errors, 0 warnings (0 minutes, 0 seconds)
+
+Process returned 0 (0x0)
+
+
+IDE
+Name: 		Code::Blocks
+Version: 	Release 12.11 rev 8692
+SDK Version:	1.13.14
+Website:	http://www.codeblocks.org
+
+使用内建 specs。
+COLLECT_GCC=g++
+COLLECT_LTO_WRAPPER=/usr/lib/gcc/x86_64-linux-gnu/4.7/lto-wrapper
+目标：x86_64-linux-gnu
+配置为：../src/configure -v --with-pkgversion='Ubuntu/Linaro 4.7.3-1ubuntu1' --with-bugurl=file:///usr/share/doc/gcc-4.7/README.Bugs --enable-languages=c,c++,go,fortran,objc,obj-c++ --prefix=/usr --program-suffix=-4.7 --enable-shared --enable-linker-build-id --libexecdir=/usr/lib --without-included-gettext --enable-threads=posix --with-gxx-include-dir=/usr/include/c++/4.7 --libdir=/usr/lib --enable-nls --with-sysroot=/ --enable-clocale=gnu --enable-libstdcxx-debug --enable-libstdcxx-time=yes --enable-gnu-unique-object --enable-plugin --with-system-zlib --enable-objc-gc --with-cloog --enable-cloog-backend=ppl --disable-cloog-version-check --disable-ppl-version-check --enable-multiarch --disable-werror --with-arch-32=i686 --with-abi=m64 --with-multilib-list=m32,m64,mx32 --with-tune=generic --enable-checking=release --build=x86_64-linux-gnu --host=x86_64-linux-gnu --target=x86_64-linux-gnu
+线程模型：posix
+gcc 版本 4.7.3 (Ubuntu/Linaro 4.7.3-1ubuntu1)
+
+************************************************/
---
+++ 01.02/result/main.cpp
@@ -0,0 +1,13 @@
+int main()
+{
+    return -1;
+}
+/**************************************
+Checking for existence: /home/fink/fink/cpp_primer_exercises/01.02/result/main
+Executing: xterm -T '/home/fink/fink/cpp_primer_exercises/01.02/result/main' -e /usr/bin/cb_console_runner "/home/fink/fink/cpp_primer_exercises/01.02/result/main" (in /home/fink/fink/cpp_primer_exercises/01.02/result)
+Process terminated with status 0 (1 minutes, 31 seconds)
+
+
+Process returned 255(0xFF)
+
+***************************************/
